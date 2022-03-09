5bcae9d48a50f12640fd9c8e1ff0b3a4005f875c
paulwainaina <paulwainaina36@gmail.com>
2022-03-09 10:14:48 +0300

    listenandservetls

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func indexHandler(w http.ResponseWriter ,r* http.Request ){
+	io.WriteString(w,"Welcome To termitarium")
+}
 
 func main(){
 	fmt.Println("Welcome")
+
+	r:=mux.NewRouter()
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	r.HandleFunc("/",indexHandler).Methods("GET").Schemes("https")
+	err:=http.ListenAndServeTLS(":8443","server.crt","server.key",r)
+	if err!=nil{
+		fmt.Println(err)
+	}
 }
