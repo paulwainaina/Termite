f73d7a07df2b7ae9d8c026a3aa6b47dd92665331
paulwainaina <paulwainaina36@gmail.com>
2022-03-09 11:35:37 +0300

    Environment

--- main.go
+++ main.go
@@ -4,21 +4,28 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func indexHandler(w http.ResponseWriter ,r* http.Request ){
+	fmt.Println(r.Header)
 	io.WriteString(w,"Welcome To termitarium")
 }
 
 func main(){
 	fmt.Println("Welcome")
+	err:=godotenv.Load()
+	if err!=nil{
+		fmt.Println("Error loading environment")
+	}
 
 	r:=mux.NewRouter()
-	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(os.Getenv("ASSETS")))))
 	r.HandleFunc("/",indexHandler).Methods("GET").Schemes("https")
-	err:=http.ListenAndServeTLS(":8443","server.crt","server.key",r)
+	err=http.ListenAndServeTLS(os.Getenv("PORT"),os.Getenv("CERT"),os.Getenv("PRIVKEY"),r)
 	if err!=nil{
 		fmt.Println(err)
 	}
